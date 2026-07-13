7d0de21c886de93771811a730c48a14f8b2a311d
CatB1t <skytedits@gmail.com>
2026-07-13 21:53:59 +0300

    refactor Shader class.

--- include/shader.hpp
+++ include/shader.hpp
@@ -9,6 +9,10 @@ class Shader {
  private:
   Handle m_program;
 
+  unsigned int m_LoadShaderSource(const std::string& source, unsigned int type);
+  void m_CheckCompileStatus(unsigned int id, const char* type);
+  void m_CheckLinkStatus(const Handle& program);
+
  public:
   Shader(const std::string& vertex_source, const std::string& fragment_source);
   void Use();
--- src/shader.cpp
+++ src/shader.cpp
@@ -10,39 +10,46 @@ namespace ptah {
 Shader::Shader(const std::string& vertex_source,
                const std::string& fragment_source)
     : m_program(glCreateProgram()) {
-  unsigned int vertex_id = glCreateShader(GL_VERTEX_SHADER);
-  unsigned int fragment_id = glCreateShader(GL_FRAGMENT_SHADER);
-  const int vs_len = vertex_source.size();
-  const int fs_len = fragment_source.size();
-  const char* src_vert = vertex_source.c_str();
-  glShaderSource(vertex_id, 1, &src_vert, &vs_len);
-  const char* src_frag = fragment_source.c_str();
-  glShaderSource(fragment_id, 1, &src_frag, &fs_len);
-  glCompileShader(vertex_id);
-  char error_msg[512];
-  int error_msg_size = 0;
-  int status;
-  glGetShaderiv(vertex_id, GL_COMPILE_STATUS, &status);
-  if (status != GL_TRUE) {
-    std::cerr << "Failed to compile shader vertex\n";
-    glGetShaderInfoLog(vertex_id, 512, &error_msg_size, error_msg);
-    std::cerr << error_msg << "\n";
-  }
-
-  glCompileShader(fragment_id);
-
-  glGetShaderiv(fragment_id, GL_COMPILE_STATUS, &status);
-  if (status != GL_TRUE) {
-    std::cerr << "Failed to compile shader vertex\n";
-    glGetShaderInfoLog(fragment_id, 512, &error_msg_size, error_msg);
-    std::cerr << error_msg << "\n";
-  }
+  unsigned int vertex_id = m_LoadShaderSource(vertex_source, GL_VERTEX_SHADER);
+  unsigned int fragment_id =
+      m_LoadShaderSource(fragment_source, GL_FRAGMENT_SHADER);
   glAttachShader(m_program.Id(), vertex_id);
   glAttachShader(m_program.Id(), fragment_id);
   glLinkProgram(m_program.Id());
-  glGetProgramiv(m_program.Id(), GL_LINK_STATUS, &status);
+  m_CheckLinkStatus(m_program);
+}
+
+unsigned int Shader::m_LoadShaderSource(const std::string& source,
+                                        unsigned int type) {
+  unsigned int id = glCreateShader(type);
+  const char* type_str = type == GL_VERTEX_ARRAY ? "vertex" : "fragment";
+  const int source_len = source.size();
+  const char* source_ptr = source.c_str();
+  glShaderSource(id, 1, &source_ptr, &source_len);
+  glCompileShader(id);
+  m_CheckCompileStatus(id, type_str);
+  return id;
+}
+
+void Shader::m_CheckCompileStatus(unsigned int id, const char* type) {
+  int status;
+  glGetShaderiv(id, GL_COMPILE_STATUS, &status);
   if (status != GL_TRUE) {
-    std::cerr << "Failed to link shader program\n";
+    char error_msg[512];
+    int error_msg_size = 0;
+    std::cerr << "Failed to compile shader of type " << type << ":\n";
+    glGetShaderInfoLog(id, 512, &error_msg_size, error_msg);
+    std::cerr << error_msg_size << error_msg[0] << "\n";
+  }
+}
+
+void Shader::m_CheckLinkStatus(const Handle& program) {
+  int status;
+  glGetProgramiv(program.Id(), GL_LINK_STATUS, &status);
+  if (status != GL_TRUE) {
+    char buf[512];
+    glGetProgramInfoLog(program.Id(), 512, nullptr, buf);
+    std::cerr << "Failed to link shader program: " << buf << "\n";
   }
 }
 
