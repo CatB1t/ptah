a310308d00f06925b220bea9cd295cdbddd86c5b
CatB1t <skytedits@gmail.com>
2026-07-16 20:36:31 +0300

    feat(utils): add Assimp model loading helper

--- include/utils/file_loading.hpp
+++ include/utils/file_loading.hpp
@@ -1,5 +1,11 @@
 #include <string>
 
+struct aiScene;
+namespace Assimp {
+class Importer;
+}
+
 namespace ptah::utils {
 std::string load_file(const char* path);
+const aiScene* load_object(Assimp::Importer& importer, const char* path);
 }  // namespace ptah::utils
--- src/utils/file_loading.cpp
+++ src/utils/file_loading.cpp
@@ -1,5 +1,9 @@
 #include "utils/file_loading.hpp"
 
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+
+#include <assimp/Importer.hpp>
 #include <fstream>
 #include <sstream>
 
@@ -17,5 +21,17 @@ std::string load_file(const char* path) {
   contents << file.rdbuf();
   return contents.str();
 }
+
+const aiScene* load_object(Assimp::Importer& importer, const char* path) {
+  const aiScene* scene = importer.ReadFile(
+      path, aiProcess_Triangulate | aiProcess_CalcTangentSpace);
+  if (scene == nullptr) {
+    PTAH_RENDER_ERROR("Assimp failed to load model {}", path);
+    PTAH_RENDER_ERROR("{}", importer.GetErrorString());
+    return nullptr;
+  }
+  return scene;
+}
+
 }  // namespace utils
 }  // namespace ptah
