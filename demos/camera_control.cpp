fe7f140923260b82a9c09a0e18d2c6d7270161a0
CatB1t <skytedits@gmail.com>
2026-07-19 05:37:03 +0300

    feat(demos): add orbit camera control demo.

---
+++ demos/camera_control.cpp
@@ -0,0 +1,47 @@
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+#include <glm/common.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <cstdlib>
+
+#include "core/context.hpp"
+#include "core/mesh.hpp"
+#include "core/renderer.hpp"
+#include "core/window.hpp"
+#include "orbit_camera.hpp"
+#include "input.hpp"
+#include "model.hpp"
+
+int main() {
+  ptah::Context ctx{};
+  if (!ctx.IsInitialized()) {
+    exit(EXIT_FAILURE);
+  }
+
+  ptah::Window window{"Demo", 1280, 720};
+
+  ptah::Renderer renderer{1280, 720};
+  window.AddResizeCallback([&] (unsigned int width, unsigned int height) {
+    renderer.Resize(width, height);
+  });
+
+  ptah::Model model {renderer, PTAH_ASSETS_DIR "/cube.fbx"};
+  glm::mat4 transform{1.0f};
+
+  ptah::OrbitCamera camera{window};
+  ptah::Input input_handler{window};
+  while (!window.ShouldClose()) {
+    input_handler.Update();
+    window.PollEvents();
+    camera.Update(input_handler);
+    double time = window.Time();
+    renderer.Begin(camera.Data(), time);
+    renderer.Submit(model.GetDrawCommands(transform));
+    renderer.Flush();
+    window.SwapBuffers();
+  }
+
+  exit(EXIT_SUCCESS);
+}
