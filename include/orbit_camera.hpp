823850d3dd31252fb5494a1d470f057fa3ad0167
CatB1t <skytedits@gmail.com>
2026-07-19 17:12:07 +0300

    feat(camera): add panning and configurable projection.

--- include/orbit_camera.hpp
+++ include/orbit_camera.hpp
@@ -13,21 +13,27 @@ class Input;
 class OrbitCamera {
  private:
   const float kPI = glm::pi<float>();
-  glm::vec3 m_position{0.0, 0.0, -1.0};
-  glm::vec3 m_target{0.0};
+  glm::vec3 m_target{0.0, 0.0, 0.0};
   float m_altitude = 0.1f;
   float m_azimuth = 0.0f;
   float m_distance = 50.0f;
-  double m_aspect_ratio = 1.0;
 
-  const float m_horizontal_speed = 0.005;
-  const float m_vertical_speed = 0.01;
+  const float m_horizontal_speed = 5.0f;
+  const float m_vertical_speed = 2.0f;
+  const float m_zoom_speed = 3.0f;
+  const float m_pan_speed = 20.0f;
+
+  double m_aspect_ratio = 1.0;
+  double m_fov;
+  double m_near;
+  double m_far;
 
   void m_Resize(double width, double height);
-  void m_Rotate(const glm::vec2& mouse_delta);
+  glm::vec3 m_CameraPos();
 
  public:
-  OrbitCamera(Window& window);
+  OrbitCamera(Window& window, double fov = 45.0, double z_near = 0.01,
+              double z_far = 500.0);
   void Update(Input& input);
   Camera Data();
 };
--- src/orbit_camera.cpp
+++ src/orbit_camera.cpp
@@ -10,45 +10,69 @@
 
 namespace ptah {
 
-OrbitCamera::OrbitCamera(Window& window) {
+OrbitCamera::OrbitCamera(Window& window, double fov, double z_near,
+                         double z_far)
+    : m_fov(fov), m_near(z_near), m_far(z_far) {
   auto size = window.Size();
-  m_aspect_ratio = size.x / static_cast<double>(size.y);
-  window.AddResizeCallback(
-      [&](double width, double height) { m_Resize(width, height); });
+  if (size.y != 0) {
+    m_aspect_ratio = size.x / static_cast<double>(size.y);
+  }
+  window.AddResizeCallback([&](unsigned int width, unsigned int height) {
+    if (height == 0) return;
+    m_Resize(width, height);
+  });
 }
 
 void OrbitCamera::m_Resize(double width, double height) {
   m_aspect_ratio = width / height;
 }
 
-void OrbitCamera::m_Rotate(const glm::vec2& mouse_delta) {
-  if (glm::length(mouse_delta) <= 0.001) return;
-  m_azimuth += mouse_delta.x * m_vertical_speed;
-  m_altitude -= mouse_delta.y * m_horizontal_speed;
-  m_azimuth = std::fmod(m_azimuth, 2.0f * kPI);
-  m_altitude = std::clamp(m_altitude, -1.0f, 1.0f);
+glm::vec3 OrbitCamera::m_CameraPos() {
+  float radius = glm::sqrt(1.01f - m_altitude * m_altitude);
+  auto cameraPos = glm::normalize(glm::vec3(
+      radius * glm::cos(m_azimuth), m_altitude, radius * glm::sin(m_azimuth)));
+  return cameraPos * m_distance + m_target;
 }
 
 void OrbitCamera::Update(Input& input) {
+  // Rotate
   if (input.IsMouseDown(MouseButton::Left)) {
-    auto delta = input.MouseDelta();
-    m_Rotate(delta);
+    auto mouse_delta = input.MouseDeltaNormalized();
+    if (glm::length(mouse_delta) > 0.0f) {
+      m_azimuth += mouse_delta.x * m_horizontal_speed;
+      m_altitude -= mouse_delta.y * m_vertical_speed;
+      m_azimuth = std::fmod(m_azimuth, 2.0f * kPI);
+      m_altitude = std::clamp(m_altitude, -1.0f, 1.0f);
+    }
   }
 
+  // Pan
+  if (input.IsMouseDown(MouseButton::Right)) {
+    auto mouse_delta = input.MouseDeltaNormalized();
+    if (glm::length(mouse_delta) > 0.0f) {
+      glm::vec3 cameraForward{glm::normalize(m_target - m_CameraPos())};
+      glm::vec3 cameraUp{0.0, 1.0, 0.0};
+      glm::vec3 cameraRight =
+          glm::normalize(glm::cross(cameraForward, cameraUp));
+      cameraUp = glm::cross(cameraForward, cameraRight);
+      m_target -= cameraRight * mouse_delta.x * m_pan_speed;
+      m_target += cameraUp * mouse_delta.y * m_pan_speed;
+    }
+  }
+
+  // Zoom
   auto scroll_delta = input.MouseScroll();
-  m_distance += scroll_delta.y;
-  m_distance = std::max(m_distance, 1.0f);
+  if (glm::length(scroll_delta) > 0.0f) {
+    m_distance += scroll_delta.y * m_zoom_speed;
+    m_distance = std::max(m_distance, 1.0f);
+  }
 }
 
 Camera OrbitCamera::Data() {
-  float radius = glm::sqrt(1.01f - m_altitude * m_altitude);
-  m_position =
-      glm::normalize(glm::vec3(radius * glm::cos(m_azimuth), m_altitude,
-                               radius * glm::sin(m_azimuth))) *
-      m_distance;
-  glm::mat4 view = glm::lookAt(m_position, m_target, glm::vec3(0.0, 1.0, 0.0));
+  glm::mat4 view =
+      glm::lookAt(m_CameraPos(), m_target, glm::vec3(0.0, 1.0, 0.0));
   glm::mat4 projection =
-      glm::perspective(glm::radians(60.0), m_aspect_ratio, 0.01, 500.0);
+      glm::perspective(glm::radians(m_fov), m_aspect_ratio, m_near, m_far);
   return Camera{view, projection};
 }
 
