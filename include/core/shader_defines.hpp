3cae97d74b10f25dd484791b158f9adead877496
CatB1t <skytedits@gmail.com>
2026-07-23 16:16:10 +0300

    fix(core): add missing shader define macros header.

---
+++ include/core/shader_defines.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+// Helper macros
+#define PTAH_STRING_IM(x) #x              // Get macro name
+#define PTAH_STRING(x) PTAH_STRING_IM(x)  // Convert macro value to string
+#define PTAH_EXPAND(x) #x " " PTAH_STRING_IM(x)  // expand to MACRO_NAME VALUE
+
+#define PTAH_N_POINT_LIGHTS 2
