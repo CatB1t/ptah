3ca5e3b4a294ecd1517fe842bf3553785360bcde
CatB1t <skytedits@gmail.com>
2026-07-23 18:11:31 +0300

    feat(logger): dedupe repeated log lines within 1s.

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -1,5 +1,6 @@
 #include "utils/logger.hpp"
 
+#include <spdlog/sinks/dup_filter_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
@@ -10,8 +11,9 @@ namespace ptah::utils {
 std::shared_ptr<spdlog::logger> Logger::s_RenderLogger;
 
 void Logger::Init() {
-  auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  s_RenderLogger = std::make_shared<spdlog::logger>("Renderer", console_sink);
+  auto duplicate_filter = std::make_shared<spdlog::sinks::dup_filter_sink_mt>(std::chrono::seconds(1));
+  duplicate_filter->add_sink(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+  s_RenderLogger = std::make_shared<spdlog::logger>("Renderer", duplicate_filter);
   s_RenderLogger->set_level(spdlog::level::debug);
 }
 
