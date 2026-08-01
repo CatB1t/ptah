c02cae82e852a1f807d6d76c925192c79f5217f4
CatB1t <skytedits@gmail.com>
2026-08-01 03:55:48 +0300

    feat(materials): set default shininess for blinn and phong

--- include/materials/materials_factory.hpp
+++ include/materials/materials_factory.hpp
@@ -18,13 +18,17 @@ inline Material MakeUnlit(const std::vector<std::string>& defines = {}) {
 }
 
 inline Material MakeBlinn(const std::vector<std::string>& defines = {}) {
-  return Material(PTAH_SHADERS_DIR "/default.vert",
-                  PTAH_SHADERS_DIR "/blinn.frag", defines);
+  auto mat = Material(PTAH_SHADERS_DIR "/default.vert",
+                      PTAH_SHADERS_DIR "/blinn.frag", defines);
+  mat.SetBlockUniform("shininess", 1.0f);
+  return mat;
 }
 
 inline Material MakePhong(const std::vector<std::string>& defines = {}) {
-  return Material(PTAH_SHADERS_DIR "/default.vert",
-                  PTAH_SHADERS_DIR "/phong.frag", defines);
+  auto mat = Material(PTAH_SHADERS_DIR "/default.vert",
+                      PTAH_SHADERS_DIR "/phong.frag", defines);
+  mat.SetBlockUniform("shininess", 1.0f);
+  return mat;
 }
 
 }  // namespace ptah
